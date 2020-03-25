a60875c00c95743a8e35a3fa4acfa175cb8e5d29
PRIYANKA SHARMA <sharmapriyanka84510@gmail.com>
2020-03-25 21:38:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.css';
+export default class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <h1 style={{textAlign:"center"}}> ToDo App </h1>
+      </div>
+    )
+  }
+}
